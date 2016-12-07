f94feb54610a27dba33c47063b0c4269d99e4efd
zadzbw <609842385@qq.com>
2016-12-07 18:10:25 +0800

    add Jest and enzyme

---
+++ src/components/Button/__test__/Button.spec.js
@@ -0,0 +1,78 @@
+/**
+ * Created by zad on 16/12/7.
+ */
+import React from 'react';
+import {mount} from 'enzyme';
+import {mountToJson} from 'enzyme-to-json';
+import Button from '../../Button';
+import Icon from '../../Icon';
+
+describe('Button test', function () {
+	it('type', () => {
+		const wrapper = mount(
+			<Button type={'primary'}>primary</Button>
+		);
+		expect(wrapper.find('button').type()).toEqual('button');
+		expect(wrapper.find('button').text()).toEqual('primary');
+		expect(wrapper.find('button').hasClass('zad-btn-primary')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('size', () => {
+		const wrapper = mount(
+			<Button size={'large'}>large</Button>
+		);
+		expect(wrapper.find('button').type()).toEqual('button');
+		expect(wrapper.find('button').text()).toEqual('large');
+		expect(wrapper.find('button').hasClass('zad-btn-lg')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('shape', () => {
+		const wrapper = mount(
+			<Button shape={'circle'}>circle</Button>
+		);
+		expect(wrapper.find('button').type()).toEqual('button');
+		expect(wrapper.find('button').text()).toEqual('circle');
+		expect(wrapper.find('button').hasClass('zad-btn-circle')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('clicked', () => {
+		let a = 1;
+
+		function test() {
+			a++;
+		}
+
+		const wrapper = mount(
+			<Button onClick={test}>button</Button>
+		);
+		expect(a).toEqual(1);
+		wrapper.find('button').simulate('click');
+		expect(a).toEqual(2);
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('disabled', () => {
+		const wrapper = mount(
+			<Button disabled>button</Button>
+		);
+
+		expect(wrapper.find('button').get(0).disabled).toBeTruthy();
+		expect(wrapper.find('button').type()).toEqual('button');
+		expect(wrapper.find('button').text()).toEqual('button');
+		expect(wrapper.find('button').hasClass('zad-btn')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('Icon', () => {
+		const wrapper = mount(
+			<Button disabled><Icon name={'shield'}/>hello</Button>
+		);
+
+		expect(wrapper.find('i').hasClass('fa-shield')).toBeTruthy();
+		expect(wrapper.find('button').text()).toEqual('hello');
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+});
---
+++ src/components/Icon/__test__/Icon.spec.js
@@ -0,0 +1,62 @@
+/**
+ * Created by zad on 16/12/7.
+ */
+import React from 'react';
+import {mount} from 'enzyme';
+import {mountToJson} from 'enzyme-to-json';
+import Icon from '../../Icon';
+
+describe('Icon test', () => {
+	it('icon name', () => {
+		const wrapper = mount(
+			<Icon name={'shield'}/>
+		);
+		expect(wrapper.find('i').hasClass('fa-shield')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('size', () => {
+		const wrapper = mount(
+			<Icon name={'shield'} size={'2x'}/>
+		);
+		expect(wrapper.find('i').hasClass('fa-shield')).toBeTruthy();
+		expect(wrapper.find('i').hasClass('fa-2x')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('fixedWidth', () => {
+		const wrapper = mount(
+			<Icon name={'shield'} fixedWidth={true}/>
+		);
+		expect(wrapper.find('i').hasClass('fa-shield')).toBeTruthy();
+		expect(wrapper.find('i').hasClass('fa-fw')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('spin', () => {
+		const wrapper = mount(
+			<Icon name={'shield'} spin={true}/>
+		);
+		expect(wrapper.find('i').hasClass('fa-shield')).toBeTruthy();
+		expect(wrapper.find('i').hasClass('fa-spin')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('pulse', () => {
+		const wrapper = mount(
+			<Icon name={'shield'} pulse={true}/>
+		);
+		expect(wrapper.find('i').hasClass('fa-shield')).toBeTruthy();
+		expect(wrapper.find('i').hasClass('fa-pulse')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('rotate', () => {
+		const wrapper = mount(
+			<Icon name={'shield'} rotate={90}/>
+		);
+		expect(wrapper.find('i').hasClass('fa-shield')).toBeTruthy();
+		expect(wrapper.find('i').hasClass('fa-rotate-90')).toBeTruthy();
+		expect(mountToJson(wrapper)).toMatchSnapshot();
+	});
+});
