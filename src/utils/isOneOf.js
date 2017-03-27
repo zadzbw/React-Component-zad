57eb62003e9b7e40ddbc35086824b9db5d3b6afb
zadzbw <609842385@qq.com>
2017-03-27 11:01:52 +0800

    add polyfill

--- src/utils/isOneOf.js
+++ src/utils/isOneOf.js
@@ -5,8 +5,23 @@
 /** verify array whether contains the target
  * @param {Array} array
  * @param {String, Number} target
+ * @param {Number} fromIndex
  * @return {Boolean}
  */
-export default function isOneOf(array, target) {
-  return Array.prototype.includes.apply(array, [target, 0]);
+export default function isOneOf(array, target, fromIndex = 0) {
+  if (!Array.prototype.includes) {
+    return includesPolyfill(array, target, fromIndex);
+  }
+  return Array.prototype.includes.apply(array, [target, fromIndex]);
+}
+
+function includesPolyfill(array, target, fromIndex = 0) {
+  const len = array.length;
+  while (fromIndex < len) {
+    if (array[fromIndex] === target) {
+      return true;
+    }
+    fromIndex++;
+  }
+  return false;
 }
