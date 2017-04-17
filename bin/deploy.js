e3a8c8fcbdb668f9cb02c175ee5a5082c99342c6
zadzbw <609842385@qq.com>
2017-04-18 00:35:39 +0800

    deploy bin

--- bin/deploy.js
+++ bin/deploy.js
@@ -4,14 +4,32 @@
 import {exec} from 'child_process';
 import _debug from 'debug';
 
+function Promisefy(fn, ctx) {
+  return function (...args) {
+    ctx = ctx || this;
+    return new Promise((resolve, reject) => {
+      fn.apply(ctx, [...args, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      }]);
+    });
+  };
+}
+
 const log = _debug('deploy:gh-pages');
 
 log('checkout branch gh-pages!!');
 
 async function deploy() {
-  const a = await exec('git checkout gh-pages');
-  console.log(a);
-  log('checkout to gh-pages');
+  try {
+    const a = await Promisefy(exec)('git checkout gh-pages');
+    console.log(a);
+    log('checkout to gh-pages');
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 deploy();
