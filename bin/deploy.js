d35c7e9cb2697a9a3caa2a136cc9129fb52129fd
zadzbw <609842385@qq.com>
2017-04-18 13:08:34 +0800

    将方法改为同步

--- bin/deploy.js
+++ bin/deploy.js
@@ -25,27 +25,26 @@ const log = _debug('deploy:gh-pages');
 async function copy(src, target) {
   try {
     const files = await Promisefy(fs.readdir)(src);
-    fs.exists(target, async (exists) => {
-      if (!exists) {
-        fs.mkdirSync(target);
+    const exists = fs.existsSync(target);
+    if (!exists) {
+      fs.mkdirSync(target);
+    }
+    log('copy file start');
+    files.forEach(async (file) => {
+      const srcPath = path.resolve(src, file);
+      const targetPath = path.resolve(target, file);
+      const stats = await Promisefy(fs.stat)(srcPath);
+      if (stats.isDirectory()) {
+        copy(srcPath, targetPath);
+      } else {
+        const readable = fs.createReadStream(srcPath);
+        // 创建写入流
+        const writable = fs.createWriteStream(targetPath);
+        // 通过管道来传输流
+        readable.pipe(writable);
       }
-      log('copy file start');
-      files.forEach(async (file) => {
-        const srcPath = path.resolve(src, file);
-        const targetPath = path.resolve(target, file);
-        const stats = await Promisefy(fs.stat)(srcPath);
-        if (stats.isDirectory()) {
-          copy(srcPath, targetPath);
-        } else {
-          const readable = fs.createReadStream(srcPath);
-          // 创建写入流
-          const writable = fs.createWriteStream(targetPath);
-          // 通过管道来传输流
-          readable.pipe(writable);
-        }
-      });
-      log('copy file done');
     });
+    log('copy file done');
   } catch (e) {
     console.log(e);
   }
@@ -55,18 +54,17 @@ async function deploy() {
   try {
     await Promisefy(exec)('git checkout gh-pages');
     log('checkout to branch gh-pages');
-    fs.exists('dist', async (exists) => {
-      if (exists) {
-        await Promisefy(exec)('rm -rf dist/*'); // commit
-      }
-      copy('build', 'dist'); // 开始读写文件
-      await Promisefy(exec)('git add dist'); // add
-      log('add files');
-      await Promisefy(exec)('git commit -a -m "deploy"'); // commit
-      log('commit info');
-      await Promisefy(exec)('git push origin gh-pages'); // push
-      log('push to gh-pages');
-    });
+    const exists = fs.existsSync('dist');
+    if (exists) {
+      await Promisefy(exec)('rm -rf dist/*'); // commit
+    }
+    copy('build', 'dist'); // 开始读写文件
+    await Promisefy(exec)('git add dist'); // add
+    log('add files');
+    await Promisefy(exec)('git commit -a -m "deploy"'); // commit
+    log('commit info');
+    await Promisefy(exec)('git push origin gh-pages'); // push
+    log('push to gh-pages');
   } catch (e) {
     console.log(e);
   }
